79f9ff2cb37c5585ccd7614fa4f9697f22c9eb05
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2024-09-07 12:00:26 -0300

    Projeto inicial

---
+++ src/main/java/com/rafaelcavalcante/encurtadorurl/EncurtadorurlApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelcavalcante.encurtadorurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorurlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelcavalcante/encurtadorurl/EncurtadorurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelcavalcante.encurtadorurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
